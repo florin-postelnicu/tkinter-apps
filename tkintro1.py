aac284833202acb163a15905dd14aa96b356d8dc
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-01-16 11:52:26 -0700

    Add files via upload

---
+++ tkintro1.py
@@ -0,0 +1,58 @@
+
+'''
+How to change the Size, Colour and Font of text in tkinter widgets
+https://www.tutorialspoint.com/python/tk_fonts.htm
+
+
+
+
+grid is used to lay out widgets in a grid.
+ Another answer says it "overlays a graph" which is a bit of a misnomer.
+ It doesn't overlay anything, it merely arranges widgets
+ along row and column boundaries. It is great for creating tables and other
+  structured types of layouts.
+
+pack lays things out along the sides of a box.
+It excels at doing layouts where everything is on a single row or
+in a single column (think rows of buttons in a toolbar or dialog box).
+It's also useful for very simple layouts such as a navigator on the left
+and a main work area on the right. It can be used
+to create very complex layouts but
+it gets tricky until you fully understand the packing algorithm.
+
+You cannot use both grid and pack with widgets that have a common parent.
+ Your app may work but it is much more likely to get into an infinite loop
+ as each manager tries to layout the widgets, then the other notices
+  the widgets change size and try to adjust, etc. etc.
+
+The third manage is place. Place is great for doing either absolute positioning
+ (ie: place widget at a given x/y)
+ or relative (eg: place a widget on the right edge of some other widget).
+
+While you cannot mix grid and pack within the same
+container (a container is typically a frame),
+you can use both grid and pack within a single application. T
+his is very, very common since each has strengths and weaknesses.
+I use both on a regular basis.
+'''
+from tkinter import*
+
+# create the root window
+
+root = Tk()
+root.title('Labeler')
+root.geometry("200x200")
+
+# create a frame in the window to hold other widgets
+
+app = Frame(root)
+app.grid()
+
+# create a label widget in the frame
+
+lbl = Label(app, text = " I'm just a simple label! Please, be kind to Me", font=('Comic Sans MS', 30," bold italic"), bg='Green', fg='blue')
+lbl.grid()
+# lbl.pack()
+
+# kick off the window's event
+root.mainloop()
